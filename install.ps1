2a36268bb1111cae85802578395acd6bd8389d07
Leetmeister <leetmeister@gmail.com>
2023-03-14 12:37:34 -0700

    Add help and nerd font install to install.ps1

--- install.ps1
+++ install.ps1
@@ -1,8 +1,39 @@
 #Requires -RunAsAdministrator
+
+<#
+.SYNOPSIS
+    Install dev experience prereqs and dotfiles.
+.DESCRIPTION
+    This script requires administrator privileges to:
+
+    Install the following apps via winget (idempotent):
+      - Git with Git Credential Manager (GCM)
+      - Starship prompt
+
+    Install the latest Caskaydia Cove Nerd Fonts
+      - See https://github.com/$Organization/$Repository/releases/latest/
+
+    Apply the following configurations:
+      - Configure GCM as default Git credential.helper (System) 
+      - Link dotfiles/.gitconfig to $HOME/.gitconfig (Global)
+      - Link dotfiles/.starship/starship.toml to $HOME/.starship/starship.toml
+         - Note: Assumes Profile.ps1 has custom $env:STARSHIP_CONFIG set for it
+
+    This script also supports ShouldProcess flags:
+      - Use -Force to execute silently and overwrite existing files/settings
+      - Use -WhatIf to simulate what this script will do
+      - Use -Confirm to prompt on each action and selectively run some of them
+.LINK
+    https://github.com/leetmeister/dotfiles
+.PARAMETER  Force
+.PARAMETER  WhatIf
+.PARAMETER  Confirm
+#>
+
 Param(
-    [Switch]$Confirm,
+    [Switch]$Force,
     [Switch]$WhatIf,
-    [Switch]$Force
+    [Switch]$Confirm
 )
 
 function Install-Git {
@@ -24,7 +55,7 @@ function Install-Git {
     # Link dotfiles\.gitconfig to local home
     Write-Host "Link dotfiles\.gitconfig to local config path ..."
     if (Test-Path "$HOME\.gitconfig") {
-        if ($Force -or $PSCmdlet.ShouldContinue("Destination: $HOME\.gitconfig", 'Overwrite with Symbolic Link')) {
+        if ($Force -or $PSCmdlet.ShouldContinue("Destination: $HOME\.gitconfig", 'Destination exists, overwrite with Symbolic Link?')) {
             New-Item -ItemType SymbolicLink -Path "$HOME\.gitconfig" -Target "$PSScriptRoot\.gitconfig" -Force:$true -WhatIf:$WhatIf -Confirm:$false
         }
     }
@@ -82,7 +113,7 @@ function Install-Starship {
     # Link dotfiles\.starship\starship.toml to local config path
     Write-Host "Link dotfiles\.starship\starship.toml to local config path ..."
     if (Test-Path "$HOME\.starship\starship.toml") {
-        if ($Force -or $PSCmdlet.ShouldContinue("Destination: $HOME\.starship\starship.toml", 'Overwrite with Symbolic Link')) {
+        if ($Force -or $PSCmdlet.ShouldContinue("Destination: $HOME\.starship\starship.toml", 'Destination exists, overwrite with Symbolic Link?')) {
             New-Item -ItemType SymbolicLink -Path "$HOME\.starship\starship.toml" -Target "$PSScriptRoot\.starship\starship.toml" -Force:$true -WhatIf:$WhatIf -Confirm:$false
         }
     }
@@ -91,6 +122,73 @@ function Install-Starship {
     }
 }
 
+function Get-LatestGitHubReleaseInfo {
+    Param(
+        $Organization,
+        $Repository
+    )
+
+    $url = "https://github.com/$Organization/$Repository/releases/latest/"
+    $request = [System.Net.WebRequest]::Create($url)
+    $response = $request.GetResponse()
+    $realTagUrl = $response.ResponseUri.OriginalString
+    $downloadUrl = $realTagUrl.Replace('tag', 'download')
+    $version = $realTagUrl.split('/')[-1].Trim('v')
+    $out = New-Object PsObject -Property @{url = $downloadUrl ; version = $version }
+    return $out
+}
+
+function Install-NerdFonts {
+    [CmdletBinding(SupportsShouldProcess)]
+    Param(
+        [Switch]$Force
+    )
+
+    if ($Force -and -not $Confirm) {
+        $ConfirmPreference = 'None'
+    }
+
+    # Download the latest Caskaydia Cove Nerd Fonts
+    $info = Get-LatestGitHubReleaseInfo -Organization ryanoasis -Repository nerd-fonts
+    $fileName = 'CascadiaCode.zip'
+    $downloadUrl = "$($info.url)/$fileName"
+    $tempFile = "$env:TEMP/$fileName"
+    $expandFolder = $fileName.Split('.')[0]
+    $tempFolder = "$env:TEMP/$expandFolder"
+
+    Write-Host "Download $downloadUrl ..."
+    if ($PSCmdlet.ShouldProcess($downloadUrl, 'Download and extract')) {
+        Invoke-WebRequest -Uri $downloadUrl -OutFile $tempFile
+        try {
+            Expand-Archive -Path $tempFile -DestinationPath $tempFolder
+
+            # Enumerate all Windows Compatible versions of font and install them
+            # See https://github.com/ryanoasis/nerd-fonts/blob/master/install.ps1 for reference
+            $fontFiles = [Collections.Generic.List[System.IO.FileInfo]]::new()
+            Get-ChildItem $tempFolder -Recurse | Where-Object {
+                ($_.Extension -match 'ttf|otf') -and ($_.BaseName -match 'Windows Compatible')
+            } | ForEach-Object {
+                $fontFiles.Add($_)
+            }
+            $fonts = $null
+            foreach ($fontFile in $fontFiles) {
+                if ($PSCmdlet.ShouldProcess($fontFile.Name, "Install Font")) {
+                    if (!$fonts) {
+                        $shellApp = New-Object -ComObject shell.application
+                        $fonts = $shellApp.NameSpace(0x14)
+                    }
+                    $fonts.CopyHere($fontFile.FullName)
+                }
+            }
+        }
+        finally {
+            Remove-Item -Path $tempFile -ErrorAction SilentlyContinue
+            Remove-Item -Path $tempFolder -Recurse -ErrorAction SilentlyContinue
+        }
+    }
+}
+
 # Invoke installation functions
-Install-Git -Force:$Force -Confirm:$Confirm -WhatIf:$WhatIf
-Install-Starship -Force:$Force -Confirm:$Confirm -WhatIf:$WhatIf
+Install-Git -Force:$Force -WhatIf:$WhatIf -Confirm:$Confirm
+Install-Starship -Force:$Force -WhatIf:$WhatIf -Confirm:$Confirm
+Install-NerdFonts -Force:$Force -WhatIf:$WhatIf -Confirm:$Confirm
